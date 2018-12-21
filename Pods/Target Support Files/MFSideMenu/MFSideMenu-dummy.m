85cbaa726a06270b70203a632090128e734a28be
Nilesh <nilesh@logisticinfotech.com>
2018-12-21 17:53:52 +0530

    Updated git ignore file

--- Pods/Target Support Files/MFSideMenu/MFSideMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MFSideMenu : NSObject
-@end
-@implementation PodsDummy_MFSideMenu
-@end
--- Pods/Target Support Files/Pods-messagingapp/Pods-messagingapp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_messagingapp : NSObject
-@end
-@implementation PodsDummy_Pods_messagingapp
-@end
